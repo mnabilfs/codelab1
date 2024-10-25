47f89e179591aad42b466163e8a1417b319845c0
mnabilfs <mnabilfs22@gmail.com>
2024-10-25 22:06:38 +0700

    change1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         System.out.println("ini perubahan nya");
+        System.out.println("check1");
         }
 }
